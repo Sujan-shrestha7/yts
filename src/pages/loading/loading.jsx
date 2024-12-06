f4c7cbaac401dbeb37e86812b26eff2cab991993
Sujanshrestha <sujanstha5732@gmail.com>
2024-12-06 15:43:28 +0545

    loading com

---
+++ src/pages/loading/loading.jsx
@@ -0,0 +1,25 @@
+import React,{useEffect, useState} from 'react'
+import Loading from './loading.gif'
+import '../Home.css'
+const loading = ({show}) => {
+    const[IsVisible,setIsVisible]=useState(true);
+    useEffect(()=>{
+        const timer = setTimeout(() => {
+    setIsVisible(false);
+    },2000);
+    // setIsVisible(true);
+    return () => {
+    clearTimeout(timer);
+    }
+    },[])
+  return (
+    <div className='loading-screen'>
+        {
+              IsVisible?<img className='loading-img' src={Loading}/>:""
+        }
+
+    </div>
+  )
+}
+
+export default loading
